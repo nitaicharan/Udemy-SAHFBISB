d0076eae00ae890b7b5e0648e01a25d5c8f8a615
Nitai Charan <nitaicharan@gmail.com>
2020-11-07 00:00:00 -0300

    134. FAQ: Use properties file to load country options

--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -9,19 +9,12 @@ public class Student {
 	
 	private String country;
 	
-	private LinkedHashMap<String, String> countryOptions;
+	private String favoriteLanguage;
+	
+	private String[] operatingSystems;
 	
 	public Student() {
 		
-		// populate country options: used ISO country code
-		countryOptions = new LinkedHashMap<>();
-		
-		countryOptions.put("BR", "Brazil");
-		countryOptions.put("FR", "France");
-		countryOptions.put("DE", "Germany");
-		countryOptions.put("IN", "India");
-		countryOptions.put("US", "United States of America");		
-
 	}
 
 	public String getFirstName() {
@@ -48,7 +41,20 @@ public class Student {
 		this.country = country;
 	}
 
-	public LinkedHashMap<String, String> getCountryOptions() {
-		return countryOptions;
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
 	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+			
 }
--- src/com/luv2code/springdemo/mvc/StudentController.java
+++ src/com/luv2code/springdemo/mvc/StudentController.java
@@ -1,5 +1,8 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/student")
 public class StudentController {
 
+    @Value("#{countryOptions}") 
+    private Map<String, String> countryOptions;
+    
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
 		
@@ -17,7 +23,10 @@ public class StudentController {
 		
 		// add student object to the model
 		theModel.addAttribute("student", theStudent);
-		
+	
+	    // add the country options to the model 
+	    theModel.addAttribute("theCountryOptions", countryOptions);
+	    
 		return "student-form";
 	}
 	

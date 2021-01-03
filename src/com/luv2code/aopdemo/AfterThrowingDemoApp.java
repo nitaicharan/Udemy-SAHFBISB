2b1e4d5089719e9f92f9f8ede973abe037bd76e5
Nitai Charan <nitaicharan@gmail.com>
2021-01-03 02:55:00 -0300

    349. AOP: @AfterThrowing

--- src/com/luv2code/aopdemo/AfterThrowingDemoApp.java
+++ src/com/luv2code/aopdemo/AfterThrowingDemoApp.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.luv2code.aopdemo.dao.AccountDAO;
 import com.luv2code.aopdemo.dao.MembershipDAO;
 
-public class AfterReturningDemoApp {
+public class AfterThrowingDemoApp {
 
 	public static void main(String[] args) {
 
@@ -19,10 +19,19 @@ public class AfterReturningDemoApp {
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 		
 		// call method to find the accounts
-		List<Account> theAccounts = theAccountDAO.findAccounts(false);
+		List<Account> theAccounts = null;
+		
+		try {
+			// add a boolean flag to simulate exceptions
+			boolean tripWire = true;
+			theAccounts = theAccountDAO.findAccounts(tripWire);
+		}
+		catch (Exception exc) {
+			System.out.println("\n\nMain Program ... caught exception: " + exc);
+		}
 		
 		// display the accounts
-		System.out.println("\n\nMain Program: AfterReturningDemoApp");
+		System.out.println("\n\nMain Program: AfterThrowingDemoApp");
 		System.out.println("----");
 		
 		System.out.println(theAccounts);
